7d71e21da31a638df855d3b91f25ff3da9cddd41
david KENEY <davidmyken@gmail.com>
2019-02-16 21:20:43 +0100

    completed splashscreen transition to login screen

--- app/components/Splashscreen/Splashscreen.js
+++ app/components/Splashscreen/Splashscreen.js
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default class Splashscreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { timer: 0 };
+    setInterval(() => {
+      this.setState({ timer: this.state.timer + 1 });
+    }, 1000);
+  }
+
   render() {
     return (
       <View style={styles.splashContainer}>
-        <Text style={styles.splashTitle}> Hello it is splashcreen </Text>
+        <Text style={styles.splashTitle}>
+          {`Welcome to Movie Check :  ${this.state.timer}`}
+        </Text>
       </View>
     );
   }
@@ -13,13 +23,14 @@ export default class Splashscreen extends Component {
 
 const styles = StyleSheet.create({
   splashContainer: {
-    backgroundColor: "green",
+    backgroundColor: "rgb(32,53,70)",
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
   },
   splashTitle: {
     fontWeight: "bold",
-    fontSize: 18
+    fontSize: 28,
+    color: "white"
   }
 });
--- index.js
+++ index.js
@@ -3,8 +3,28 @@
 import { AppRegistry } from "react-native";
 //import App from "./App";
 import { name as appName } from "./app.json";
+import React, { Component } from "react";
 
-//import Splashscreen from "./app/components/Splahscreen/Splashscreen";
-import log from "./app/components/log";
+import Splashscreen from "./app/components/Splashscreen/Splashscreen";
+import Log from "./app/components/Log";
 
-AppRegistry.registerComponent(appName, () => log);
+class Starter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentScreen: "Splashscreen" };
+    console.log("Start doing somme task for about 3 seconds");
+
+    setTimeout(() => {
+      this.setState({ currentScreen: "Log" });
+    }, 3000);
+  }
+
+  render() {
+    let { currentScreen } = this.state;
+    let mainScreen =
+      currentScreen === "Splashscreen" ? <Splashscreen /> : <Log />;
+    return mainScreen;
+  }
+}
+
+AppRegistry.registerComponent(appName, () => Starter);
